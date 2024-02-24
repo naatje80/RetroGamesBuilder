df3126f05f18d8ea3bd94d94d5bf9053d43f91b3
naatje80 <spekbukkem@gmail.com>
2024-02-24 20:20:35 +0100

    Fixed font rendering scummvm

--- Emulators/Scummvm/AGS/scripts/compile.sh
+++ Emulators/Scummvm/AGS/scripts/compile.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 set -e
 
+PNGVERSION=1.6.43
 SDL2VERSION=2.30.0
-ZLIBNGVERSION=2.1.6
+ZLIBVERSION=1.3.1
+OGGVERSION=1.3.5
 SCUMMVMVERSION=2.8.0
 VORBISVERSION=1.3.7
 FREETYPEVERSION=2.13.2
@@ -11,8 +13,14 @@ FREETYPEVERSION=2.13.2
 if [[ ! -d /buildenv ]]; then mkdir /buildenv; fi
 cd /buildenv
 
-# First required to install sub-depencies. Some dependies will be overwritten below by statically compiled version
-pacman -Suy --noconfirm libpipewire libpulse libglvnd
+# Required to install sub-depencies. Some dependies will be overwritten below by statically compiled version
+pacman -Suy --noconfirm libpipewire libpulse libglvnd 
+
+git clone --depth 1 --branch v${PNGVERSION} https://github.com/pnggroup/libpng.git
+cd libpng
+./configure --prefix=/usr --disable-shared --enable-static 
+make -j $(nproc) install
+cd /buildenv
 
 git clone --depth=1 --branch release-${SDL2VERSION} https://github.com/libsdl-org/SDL.git
 cd SDL
@@ -20,11 +28,17 @@ cd SDL
 make -j $(nproc) install
 cd /buildenv
 
+git clone --depth=1 --branch v${OGGVERSION} https://github.com/xiph/ogg.git
+cd ogg
+./autogen.sh && ./configure --prefix=/usr --disable-shared --enable-static 
+make -j $(nproc) install
+cd /buildenv
+
 pacman -Syu --noconfirm cmake
-git clone --depth=1 --branch ${ZLIBNGVERSION} https://github.com/zlib-ng/zlib-ng.git
-cd zlib-ng
-cmake -B build -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=OFF
-make -C build -j $(nproc) install
+git clone --depth=1 --branch v${ZLIBVERSION} https://github.com/madler/zlib.git
+cd zlib
+./configure --prefix=/usr --static 
+make -j $(nproc) install
 cd /buildenv
 
 git clone --depth=1 --branch v${VORBISVERSION} https://github.com/xiph/vorbis.git
@@ -41,20 +55,31 @@ cd /buildenv
 
 git clone --depth=1 --branch v${SCUMMVMVERSION} https://github.com/scummvm/scummvm.git
 cd scummvm
+patch -p1 < /scripts/gobliiins5_detection.patch
 mkdir build
 cd build
-../configure --enable-static \
+
+../configure --enable-release \
+	--enable-static \
 	--disable-all-engines \
 	--disable-mt32emu \
 	--disable-lua \
+	--disable-discord \
+	--disable-cloud \
+	--disable-flac \
+	--disable-libcurl \
+	--disable-alsa \
 	--enable-engines=${ENGINE}
+sed -i 's/-lpng16/-l:libpng16.a/' config.mk
 sed -i 's/-lSDL2/-l:libSDL2.a/' config.mk
 sed -i 's/-ljpeg/-l:libjpeg.a/' config.mk
 sed -i 's/-lz/-l:libz.a/' config.mk
+sed -i 's/-logg/-l:libogg.a/' config.mk
 sed -i 's/-lvorbisfile/-l:libvorbisfile.a/' config.mk
 sed -i 's/-lvorbis/-l:libvorbis.a/' config.mk
 make -j $(nproc) V=1
 strip scummvm
 cp scummvm /dist
 cp -r ../gui/themes /dist
+cp ../dists/engine-data/fonts.dat /dist
 cd /buildenv
